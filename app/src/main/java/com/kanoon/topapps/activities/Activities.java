1450f5c2ec4eebb1ad23dc2abf66de73c4513480
ehsan souri <ehsansouri23@gmail.com>
2018-03-29 11:57:30 +0430

    activities class and layout added

---
+++ app/src/main/java/com/kanoon/topapps/activities/Activities.java
@@ -0,0 +1,24 @@
+package com.kanoon.topapps.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.kanoon.topapps.R;
+
+public class Activities extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.activity_activites, container, false);
+        return view;
+    }
+}
