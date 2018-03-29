ab38478b19c96b9ceb6d44dd5e3f7e58a524fcc9
ehsan souri <ehsansouri23@gmail.com>
2018-03-29 11:48:58 +0430

    constants classes added

--- app/src/main/java/com/kanoon/topapps/consts/Labels.java
+++ app/src/main/java/com/kanoon/topapps/consts/Labels.java
@@ -11,102 +11,105 @@ import java.net.ContentHandler;
  */
 
 public class Labels {
-    public static String getLabel(Context contex, int state) {
+    public static String getLabel(Context context, int state) {
         String label;
 
         switch (state) {
             case State.STATE_TASKS_MAIN:
-                label = contex.getResources().getString(R.string.tasks_main);
+                label = context.getResources().getString(R.string.tasks_main);
                 break;
             case State.STATE_TASK_SUBLEVEL:
-                label = contex.getResources().getString(R.string.task_sublevel);
+                label = context.getResources().getString(R.string.task_sublevel);
                 break;
             case State.STATE_TASK_TYPE:
-                label = contex.getResources().getString(R.string.task_type);
+                label = context.getResources().getString(R.string.task_type);
                 break;
             case State.STATE_GROUP_MAIN_CODE:
-                label = contex.getResources().getString(R.string.group_main_code);
+                label = context.getResources().getString(R.string.group_main_code);
                 break;
             case State.STATE_GROUP_CODE:
-                label = contex.getResources().getString(R.string.group_code);
+                label = context.getResources().getString(R.string.group_code);
                 break;
             case State.STATE_COURSE_ID:
-                label = contex.getResources().getString(R.string.course_id);
+                label = context.getResources().getString(R.string.course_id);
                 break;
             case State.STATE_TEST_DATE:
-                label = contex.getResources().getString(R.string.test_date);
+                label = context.getResources().getString(R.string.test_date);
                 break;
             case State.STATE_BOOKS:
-                label = contex.getResources().getString(R.string.books);
+                label = context.getResources().getString(R.string.books);
                 break;
             case State.STATE_VERSION:
-                label = contex.getResources().getString(R.string.version);
+                label = context.getResources().getString(R.string.version);
                 break;
             case State.STATE_LOCATION:
-                label = contex.getResources().getString(R.string.location);
+                label = context.getResources().getString(R.string.location);
                 break;
             case State.STATE_CHANNEL_ID:
-                label = contex.getResources().getString(R.string.channel_id);
+                label = context.getResources().getString(R.string.channel_id);
                 break;
             case State.STATE_TEST_TYPE:
-                label = contex.getResources().getString(R.string.test_type);
+                label = context.getResources().getString(R.string.test_type);
                 break;
             case State.STATE_CATEGORY:
-                label = contex.getResources().getString(R.string.category);
+                label = context.getResources().getString(R.string.category);
                 break;
             case State.STATE_CRS_SUBJECT:
-                label = contex.getResources().getString(R.string.crs_subject);
+                label = context.getResources().getString(R.string.crs_subject);
                 break;
             case State.STATE_SUBJECT:
-                label = contex.getResources().getString(R.string.subject);
+                label = context.getResources().getString(R.string.subject);
                 break;
             case State.STATE_SESSION_DATE:
-                label = contex.getResources().getString(R.string.session_date);
+                label = context.getResources().getString(R.string.session_date);
                 break;
             case State.STATE_SESSION_LOCATION:
-                label = contex.getResources().getString(R.string.session_location);
+                label = context.getResources().getString(R.string.session_location);
                 break;
             case State.STATE_SESSION_MANAGER:
-                label = contex.getResources().getString(R.string.session_manager);
+                label = context.getResources().getString(R.string.session_manager);
                 break;
             case State.STATE_TOTAL_CALL_COUNT:
-                label = contex.getResources().getString(R.string.total_call_count);
+                label = context.getResources().getString(R.string.total_call_count);
                 break;
             case State.STATE_SUCCESSFUL_CALL_COUNT:
-                label = contex.getResources().getString(R.string.successful_call_count);
+                label = context.getResources().getString(R.string.successful_call_count);
                 break;
             case State.STATE_UNSUCCESSFUL_CALL_COUNT:
-                label = contex.getResources().getString(R.string.unsuccessful_call_count);
+                label = context.getResources().getString(R.string.unsuccessful_call_count);
                 break;
             case State.STATE_OWNER:
-                label = contex.getResources().getString(R.string.owner);
+                label = context.getResources().getString(R.string.owner);
                 break;
             case State.STATE_INTERVIEWEE:
-                label = contex.getResources().getString(R.string.interviewee);
+                label = context.getResources().getString(R.string.interviewee);
                 break;
             case State.STATE_LEARN_SOURCE:
-                label = contex.getResources().getString(R.string.learn_course);
+                label = context.getResources().getString(R.string.learn_course);
                 break;
             case State.STATE_DESCRIPTION:
-                label = contex.getResources().getString(R.string.description);
+                label = context.getResources().getString(R.string.description);
                 break;
             case State.STATE_COUNT:
-                label = contex.getResources().getString(R.string.count);
+                label = context.getResources().getString(R.string.count);
                 break;
             case State.STATE_PAGE_COUNT:
-                label = contex.getResources().getString(R.string.page_count);
+                label = context.getResources().getString(R.string.page_count);
                 break;
             case State.STATE_QUESTION_COUNT:
-                label = contex.getResources().getString(R.string.question_count);
+                label = context.getResources().getString(R.string.question_count);
                 break;
             case State.STATE_TIME:
-                label = contex.getResources().getString(R.string.time);
+                label = context.getResources().getString(R.string.time);
                 break;
             case State.STATE_GENERATE_DATE:
-                label = contex.getResources().getString(R.string.generate_date);
+                label = context.getResources().getString(R.string.generate_date);
                 break;
             case State.STATE_SEND_DATE:
-                label = contex.getResources().getString(R.string.send_date);
+                label = context.getResources().getString(R.string.send_date);
+                break;
+            case State.STATE_MANAGER_USER_ID:
+                label = context.getResources().getString(R.string.manager);
                 break;
             default:
                 label = "...";
